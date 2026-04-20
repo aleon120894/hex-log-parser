fcafff3c88b82493ccc0b00f398f09a0ab691043
aleon120894 <aleon120894@gmail.com>
2026-04-20 18:05:46 +0300

    First commit: writing project base

---
+++ parser/decoder.py
@@ -0,0 +1,9 @@
+from parser.models import Packet
+
+def decode_packets(hex_lines):
+    packets = []
+    for line in hex_lines:
+        raw_bytes = bytes.fromhex(line)
+        packet = Packet(raw_bytes)
+        packets.append(packet)
+    return packets
---
+++ utils/hex_utils.py
@@ -0,0 +1,3 @@
+
+def hex_to_bytes(hex_string):
+    return bytes.fromhex(hex_string)
